cbadc13a3cab1755bcc05026783bb1751ddc180c
Sharanabasappa Hagargi <shagargi@epicor.com>
2021-06-26 19:42:38 +0530

    added locator, pageactions for prodcut page

--- src/main/java/com/qa/qs/pages/ProductPage.java
+++ src/main/java/com/qa/qs/pages/ProductPage.java
@@ -1,13 +1,19 @@
 package com.qa.qs.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 import com.qa.qs.base.BasePage;
 
 public class ProductPage extends BasePage {
-	
+	WebDriver driver;
+	By prodId = By.id("prodid");
 	public  ProductPage() {
 		System.out.println("This is a product page");
-		System.out.println("This is a product page");
+		driver = this.driver;
 		
 	}
-
+public String getTitle() {
+	return driver.getTitle();
+}
 }
