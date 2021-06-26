3eaa90c7a95f7c74554b89fcb672d1a288857f95
Sharanabasappa Hagargi <shagargi@epicor.com>
2021-06-26 18:59:13 +0530

    added product page

---
+++ src/main/java/com/qa/qs/pages/ProductPage.java
@@ -0,0 +1,12 @@
+package com.qa.qs.pages;
+
+import com.qa.qs.base.BasePage;
+
+public class ProductPage extends BasePage {
+	
+	public  ProductPage() {
+		System.out.println("This is a product page");
+		
+	}
+
+}
